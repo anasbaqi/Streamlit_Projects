7de2e2f749ea5d61897f770e3dc041f309f3fd75
anasbaqi <50376856+anasbaqi@users.noreply.github.com>
2023-07-27 22:32:26 +0300

    Add files via upload

--- bank_analysis.py
+++ bank_analysis.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 # Function to make list into a markdown for easier viewability:
 def list_to_markdown(selected_data):
+    st.write('Your Selected Datasets:')
     s = ''
     for i in selected_data:
         s += "- " + i + "\n"
@@ -18,29 +19,13 @@ def search_web(topic):
     st.write(num_of_results, "results")
     if num_of_results == 0: 
         st.write('Try a different topic (maybe capitalize / uncapitalize word)')
+
     return search_results
 
-def select_data(search_results):
-    #form for multi-selecting data from search:
-    with st.form("multi-select data"):
-        selected_data = st.multiselect('View and select data from results', search_results)
-        submitted = st.form_submit_button("done selecting data")
-    
-    if submitted:
-        st.write('You picked these data:')
-        list_to_markdown(selected_data)
 
-    return selected_data
-
-def select_topic():
-    #New form where user will select their topic:
-    topic_form =  st.form("enter topic")
-    topic = topic_form.text_input("Select a topic to start searching for relevant data: ")
-    exec_done = topic_form.form_submit_button("Submit")
-
-    if exec_done == True: 
-        st.write("the topic you selected is: ", topic)
-    return topic, exec_done
+@st.cache_data
+def metadata_info(id):
+    return wb.series.metadata.get(id)
 
 
 def main():
@@ -49,31 +34,43 @@ def main():
     st.header(" World Bank Indicator Simple Search ")
     st.subheader("A work in progress by Anas AbdulBaqi :construction_worker:")
 
-    #call select_topic to grab the topic from user:
-    topic, exec_done = select_topic()
+    with st.sidebar:
+        #User picks his topic
+        topic_form = st.form("topic")
+        topic = topic_form.text_input("Pick a topic to search:")
+
+        #Search API for the results:
+        if topic != "":
+            search_results = search_web(topic)
+            search_results_value = [item[   'value'] for item in search_results.items]
+        topic_form.form_submit_button("Start the search")
+
+        #User selects data from the results
+        text_form = st.form("form")
+        if topic != "":
+            val = text_form.multiselect("select your data from the results:", search_results_value)
+            text_form.form_submit_button("Finished selecting data")
+
+            new_results = [item for item in search_results.items if item['value'] in val]
+            new_results_id = [item['id'] for item in new_results]
+
+            
+
+
+
+
+    st.header('Info about your data')
+    #Display the results
+    if topic != "":
+        list_to_markdown(val)
+
+
+    #Option for more info about the data
     
-    start_select = False
-    if exec_done:
-        #call search_web to search the API with topic and output the number of results after form is submitted
-        search_results = search_web(topic)
-        start_select = True
-    
-    selected_data = []
-
-    if start_select == True:
-        # #form to ask user if he is ready:
-        done_selecting = False
-        search_results_value = [item['value'] for item in search_results.items]
-        selected_data = select_data(search_results_value)
-        st.write(selected_data)
-
-    if selected_data is not None and selected_data != []:
-            st.write('THIS IS BAD')
-
-    # #Show data:
-    # if data_selected == True:
-    #     st.write('You picked these data:')
-    #     list_to_markdown(selected_data)
+        for id in new_results_id:
+            id_info = metadata_info(id)
+            with st.expander("more info about data"):
+                id_info
 
 if __name__ == "__main__":
     main()
